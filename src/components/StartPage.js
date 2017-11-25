91dcff35f6b51a3299d01f4fa4e5774e0babada7
jamestthompson3 <jamestthompson3@gmail.com>
2017-11-25 17:10:28 +0200

    [+] Adds start countdown

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,6 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-const StartPage = () => <h1>START</h1>
 
+const StartWrapper = styled.div`
+  margin: auto;
+  h1 {
+    text-align: center;
+  }
+`
+class StartPage extends Component {
+  state = {
+    countdown: 3,
+    countdownDisplay: true
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.startCountdown(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  startCountdown = () => {
+    let { countdown } = this.state
+    countdown = countdown -1
+    this.setState({ countdown })
+    setTimeout(() => this.clearCountdown(), 1600)
+  }
+
+  clearCountdown = () => {
+    clearInterval(this.timerID)
+    this.setState({ countdownDisplay: false })
+  }
+
+  render() {
+    const { countdown, countdownDisplay } = this.state
+    return (
+      <StartWrapper>
+        { countdownDisplay &&
+          <h1 style={{ color: 'white' }}>{countdown}</h1>
+        }
+        { countdownDisplay ||
+          <h1 style={{ color: 'white' }}>Go!</h1>
+        }
+      </StartWrapper>
+    )
+  }
+}
 
 export default StartPage
